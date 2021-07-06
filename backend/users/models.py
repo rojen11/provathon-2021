beb2c365df2e0da5812df4801e22b7a8fd5345e7
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-06 14:04:13 +0545

    feat: user authentication

---
+++ backend/users/models.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, isTeacher, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            isTeacher=isTeacher
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, email, first_name, last_name, isTeacher, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            isTeacher=isTeacher
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    username = models.CharField(blank=True, max_length=254)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    isTeacher = models.BooleanField(default=False, blank=False)
+    first_name = models.CharField(_('first name'), max_length=150, blank=False)
+    last_name = models.CharField(_('last name'), max_length=150, blank=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['isTeacher', 'first_name', 'last_name']
+    EMAIL_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
