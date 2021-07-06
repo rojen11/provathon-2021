6728d58ad286012fa1d5464707ffb7576eff4752
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-06 19:54:12 +0545

    feat: desktop app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
 import PageSetup from "./PageSetup";
 
+try {
+  const electron = window.require('electron');
+  const fs = electron.remote.require('fs');
+  const ipcRenderer  = electron.ipcRenderer;
+
+  window.desktop = true;
+
+} catch {
+  console.log('browser');
+}
+
 function App() {
   return (
     <div className="App">
