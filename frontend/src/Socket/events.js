f6c838d769ee8c9f3755ab8a45eb1654aa0aaa0f
PratikSilwal111 <pratiksilwal111@gmail.com>
2021-07-06 19:18:48 +0545

    Update

--- frontend/src/Socket/events.js
+++ frontend/src/Socket/events.js
@@ -4,11 +4,13 @@ import * as ActionType from "../Store/actionTypes";
 
 let socket;
 let examID;
+let userID;
 
 export function ConnectSocket(id) {
   socket = io("http://localhost:5000");
   store.dispatch({ type: ActionType.SOCKET_STORE, socket: socket });
   examID = id;
+  userID = store.getState().AuthReducer.userID;
   ExamStart(id);
   events();
 }
@@ -28,6 +30,12 @@ export function TicketOpened(ticketID, title, body, studentID) {
     .emit("ticket-open", ticketID, title, body, studentID, (res) => {
       console.log(res);
     });
+    store.dispatch({type: ActionType.ADD_TICKET, ticket: { id: ticketID,
+      title: title,
+      body: body,
+      closed: false,
+      comments: []}
+    })
 }
 
 export function ReplyToTicket(reply, studentID, ticketID) {
@@ -35,18 +43,26 @@ export function ReplyToTicket(reply, studentID, ticketID) {
 }
 
 export function CloseTicket(ticketID, teacherName) {
-  socket.emit("ticket-closed", ticketID, teacherName, (res) => {
+  socket.to(`${userID}`).emit("ticket-closed", ticketID, teacherName);
     //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
-  });
 }
 
 export function events() {
   socket.on("ticket-open-teacher", (ticketID, title, body, studentID) => {
     console.log(ticketID, title, body, studentID);
+    store.dispatch({type: ActionType.ADD_TICKET, ticket: { id: ticketID,
+      title: title,
+      body: body,
+      closed: false,
+      comments: [],
+      studentID: studentID
+    }
+    })
   });
 
   socket.on("ticket-closed-by-teacher", (ticketID, teacherName) => {
     console.log(ticketID, teacherName);
+      //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
   });
 
   socket.on("ticket-reply", (ticketID, name, reply) => {
