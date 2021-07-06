8066f8d919bb355007a6537cbe51140daeff066d
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-06 16:39:21 +0545

    feat:  exam api

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -3,10 +3,13 @@ import graphene
 # Authentication Schemas
 from users.schema import AuthMutation, AuthQuery
 
-class Mutation(AuthMutation, graphene.ObjectType):
+from courses.schema import Query as CourseQuery, Mutation as CourseMutation
+from exams.schema import Query as ExamQuery, Mutation as ExamMutation
+
+class Mutation(AuthMutation, CourseMutation, ExamMutation, graphene.ObjectType):
    pass
 
-class Query(AuthQuery, graphene.ObjectType):
+class Query(AuthQuery, CourseQuery, ExamQuery, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ backend/exams/schema.py
@@ -0,0 +1,76 @@
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from exams.models import Exam
+from users.models import User
+from courses.models import CourseUser, Course
+
+import graphene
+
+
+class ExamType(DjangoObjectType):
+    class Meta:
+        model = Exam
+        
+
+class CreateExam(graphene.Mutation):
+
+    # model
+    exam = graphene.Field(ExamType)
+    
+    class Arguments:
+        name = graphene.String(required=True)
+        course_id = graphene.ID()
+        start_time = graphene.DateTime()
+        end_time = graphene.DateTime()
+        submit_duration = graphene.Int()
+        total_marks = graphene.Int()
+
+    @classmethod
+    def mutate(cls, root, info, name, course_id, start_time, end_time, submit_duration, total_marks):
+        user = info.context.user
+
+        if (user.is_authenticated):
+
+            if (not user.is_teacher):
+                raise Exception("You must be a teacher to perform this task!")
+            
+            # check if the user has the course
+            course = Course.objects.filter(id = course_id, primary_teacher=user)
+            
+            if (len(course) == 1):
+                exam = Exam(name=name, course=course.first(), start_time=start_time, end_time=end_time, submit_duration=submit_duration, total_marks=total_marks, completed=False)
+                exam.save()
+
+                return CreateExam(exam=exam)
+
+            raise Exception("Course not found!")
+
+        raise Exception("You must be logged in!")
+
+
+
+
+class Query(graphene.ObjectType):
+
+    exams_by_course = graphene.List(ExamType, id=graphene.ID())
+
+
+
+    def resolve_exams_by_course(self, info, id):
+        user = info.context.user
+        
+        if (user.is_authenticated):
+            return Exam.objects.raw("SELECT * FROM exams_exam LEFT JOIN courses_courseuser ON\
+                                    (exams_exam.course_id = courses_courseuser.course_id) LEFT JOIN users_user ON\
+                                    (courses_courseuser.user_id = users_user.id) WHERE users_user.id = %s AND exams_exam.course_id=%s",
+                                    [user.id, id])
+
+
+        else:
+            raise Exception("You must be logged in!")
+
+
+class Mutation(graphene.ObjectType):
+    create_exam = CreateExam.Field()
+
