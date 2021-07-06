6f67d391c0d102537a0348f00c5543f119a8d911
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-06 21:35:22 +0545

    feat: desktop app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,32 @@
 import PageSetup from "./PageSetup";
 
+import { connect } from "react-redux";
+import { useEffect } from "react";
+
+// Log system
+
+let ipcRenderer = null;
+
 try {
-  const electron = window.require('electron');
-  const fs = electron.remote.require('fs');
-  const ipcRenderer  = electron.ipcRenderer;
+  const electron = window.require("electron");
+  const fs = electron.remote.require("fs");
+   ipcRenderer = electron.ipcRenderer;
 
   window.desktop = true;
-
 } catch {
-  console.log('browser');
+  console.log("browser");
 }
 
-function App() {
+function App({ dispatch }) {
+  useEffect(() => {
+    console.log(window.desktop)
+    if (window.desktop) {
+      ipcRenderer.on("asynchronous-message", function (evt, message) {
+        // dispatch({type: })
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
       <PageSetup />
@@ -19,4 +34,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect()(App);
--- frontend/src/Socket/events.js
+++ frontend/src/Socket/events.js
@@ -7,7 +7,7 @@ let examID;
 let userID;
 
 export function ConnectSocket(id) {
-  socket = io("http://localhost:5000");
+  socket = io("http://localhost:5000", { path: "/socket" });
   store.dispatch({ type: ActionType.SOCKET_STORE, socket: socket });
   examID = id;
   userID = store.getState().AuthReducer.userID;
@@ -30,12 +30,16 @@ export function TicketOpened(ticketID, title, body, studentID) {
     .emit("ticket-open", ticketID, title, body, studentID, (res) => {
       console.log(res);
     });
-    store.dispatch({type: ActionType.ADD_TICKET, ticket: { id: ticketID,
+  store.dispatch({
+    type: ActionType.ADD_TICKET,
+    ticket: {
+      id: ticketID,
       title: title,
       body: body,
       closed: false,
-      comments: []}
-    })
+      comments: [],
+    },
+  });
 }
 
 export function ReplyToTicket(reply, studentID, ticketID) {
@@ -44,25 +48,28 @@ export function ReplyToTicket(reply, studentID, ticketID) {
 
 export function CloseTicket(ticketID, teacherName) {
   socket.to(`${userID}`).emit("ticket-closed", ticketID, teacherName);
-    //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
+  //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
 }
 
 export function events() {
   socket.on("ticket-open-teacher", (ticketID, title, body, studentID) => {
     console.log(ticketID, title, body, studentID);
-    store.dispatch({type: ActionType.ADD_TICKET, ticket: { id: ticketID,
-      title: title,
-      body: body,
-      closed: false,
-      comments: [],
-      studentID: studentID
-    }
-    })
+    store.dispatch({
+      type: ActionType.ADD_TICKET,
+      ticket: {
+        id: ticketID,
+        title: title,
+        body: body,
+        closed: false,
+        comments: [],
+        studentID: studentID,
+      },
+    });
   });
 
   socket.on("ticket-closed-by-teacher", (ticketID, teacherName) => {
     console.log(ticketID, teacherName);
-      //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
+    //store.dispatch({ type: ActionType.CLOSE_TICKET, id: ticketID });
   });
 
   socket.on("ticket-reply", (ticketID, name, reply) => {
@@ -74,4 +81,15 @@ export function events() {
       comment: reply,
     });
   });
+}
+
+// Logger
+export function sendLog(message) {
+  console.log(message);
+  if (socket != null) {
+    socket.emit(
+      "log",
+      message
+    );
+  }
 }
