d078d820dcef8fea4c65ae557a494b5ba3d5c249
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-07-07 07:22:27 +0545

    feat: added log page

--- frontend/src/Pages/StudentLog/index.js
+++ frontend/src/Pages/StudentLog/index.js
@@ -1,16 +1,46 @@
-import { useParams } from "react-router-dom";
-import { useEffect } from 'react';
+import { useHistory, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import AxiosFunction from "../../Store/axiosFunction";
 
 export default function StudentLog() {
   const { examid } = useParams();
 
-    useEffect(() => {
-        
-    },[])
+  const [logs, setLogs] = useState([]);
+  const [examName, setExamName] = useState("");
+
+  const history = useHistory();
+
+  useEffect(() => {
+    AxiosFunction(`query {
+        logsByExam (id: ${examid}) {
+            id
+            student {
+                firstName
+                lastName
+            }
+            exam {
+                name
+            }
+            message
+            createdAt
+        }
+    }`).then((res) => {
+      if (res !== null) {
+        const logsByExam = res.data.data.logsByExam;
+        console.log(logsByExam)
+        if (logsByExam.length > 0) {
+          setLogs(logsByExam);
+          setExamName(logsByExam[0].exam.name)
+        }
+      } else {
+        history.push("/");
+      }
+    });
+  }, []);
 
   return (
     <div className="flex flex-row gap-4 p-4 relative min-h-screen">
-      <div>
+      <div className="cursor-pointer" onClick={() => history.push("/")}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-10 w-10"
@@ -21,9 +51,13 @@ export default function StudentLog() {
         </svg>
       </div>
       <div className="w-full">
-        <h3 className="text-center font-bold text-5xl">{examid}</h3>
-        <div className="mt-5 shadow-lg rounded-lg border-2 border-gray-100 h-4/5">
-          Hello world
+        <h3 className="text-center font-bold text-5xl">{examName}</h3>
+        <div className="mt-5 shadow-lg rounded-lg border-2 border-gray-100 overflow-y-auto">
+          {logs.map((log) => (
+            <p>
+              [{log.createdAt}] {log.student.firstName} {log.student.lastName} action: {log.message}
+            </p>
+          ))}
         </div>
       </div>
     </div>
